20e0bdf156d140cb01eedf11cd34d0ed64057635
Regiany Almeida <regiany.nunes@ibm.com>
2020-05-19 22:03:43 -0300

    stack pops same one pushed

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,18 +1,20 @@
 const stackFactory = () => {
-  let empty = true;
   let count = 0;
+  let element;
 
 
   return {
     isEmpty: () => count === 0,
     size: () => count,
-    push: () => {
+    push: (ele) => {
       if (count === 2) throw new Error('capacity overflow error');
       count += 1;
+      element = ele;
     },
     pop: () => {     
       if (count === 0) throw new Error('capacity underflow error');   
       count -= 1;
+      return element
     }
   }
 };
@@ -67,7 +69,10 @@ describe('a stack', () => {
       stack.pop();
     }).toThrowError('capacity underflow error');
   });
-  it.todo('pops the same one item when pushed');
+  it('pops the same one item when pushed', () => {
+    stack.push('a');
+    expect(stack.pop()).toBe('a');
+  });
   it.todo('pops two items with the most recent first');
   it.todo('accepts only a positive capacity');
 });
