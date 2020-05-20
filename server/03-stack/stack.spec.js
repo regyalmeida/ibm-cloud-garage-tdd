36d0ce86eb0a3848b9fa59914772459d15023863
Regiany Almeida <regiany.nunes@ibm.com>
2020-05-19 22:08:29 -0300

    stack kata complete

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,11 +1,12 @@
-const stackFactory = () => {
+const stackFactory = (capacity) => {
+  if (capacity < 1) throw new Error('invalid capacity');
   let elements = [];
 
   return {
     isEmpty: () => elements.length === 0,
     size: () => elements.length,
     push: (element) => {
-      if (elements.length === 2) throw new Error('capacity overflow error');     
+      if (elements.length === capacity) throw new Error('capacity overflow error');     
       elements.push(element);
     },
     pop: () => {     
@@ -26,7 +27,7 @@ describe('the stack canary spec', () => {
 
 describe('a stack', () => {
   beforeEach(() => { 
-    stack = stackFactory();
+    stack = stackFactory(2);
   })
 
   it('starts empty', () => {
@@ -75,5 +76,9 @@ describe('a stack', () => {
     expect(stack.pop()).toBe('2');
     expect(stack.pop()).toBe('1');
   });
-  it.todo('accepts only a positive capacity');
+  it('accepts only a positive capacity', () => {
+    expect(() => {
+      stack = stackFactory(-1);
+    }).toThrowError('invalid capacity');
+  });
 });
