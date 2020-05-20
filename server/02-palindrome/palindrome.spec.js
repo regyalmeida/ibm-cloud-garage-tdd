86dfab6c13a6c8f0ac88dfabbbeabc5d8235d306
Regiany Almeida <regiany.nunes@ibm.com>
2020-05-19 21:11:54 -0300

    substitute negative test for positive one

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,5 +1,5 @@
-let isPalindrome = (word) => {
-  return word === word.split('').reverse().join('')
+let isPalindrome = (phrase) => {
+  return phrase === phrase.split('').reverse().join('')
 };
 
 
@@ -13,8 +13,9 @@ describe('the palindrome canary spec', () => {
   it('false for dude', () => {
     expect(isPalindrome('dude')).toBe(false);
   });
-  it.todo('true for mom mom');
-  it.todo('false for dad mom');
+  it('false for dad mom', () => {
+    expect(isPalindrome('dad mom')).toBe(false);
+  });
   it.todo('true for whitespace');
   it.todo('error for empty string');
   it.todo('error for not a string');
