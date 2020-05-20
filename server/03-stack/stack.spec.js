deb60fdc32310430989da11c55518864d4544a69
Regiany Almeida <regiany.nunes@ibm.com>
2020-05-19 22:01:55 -0300

    stack throws underflow error

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -10,7 +10,8 @@ const stackFactory = () => {
       if (count === 2) throw new Error('capacity overflow error');
       count += 1;
     },
-    pop: () => {        
+    pop: () => {     
+      if (count === 0) throw new Error('capacity underflow error');   
       count -= 1;
     }
   }
@@ -61,7 +62,11 @@ describe('a stack', () => {
       stack.push();
     }).toThrowError('capacity overflow error');
   });
-  it.todo('throw underflow error when popping an empty stack');
+  it('throw underflow error when popping an empty stack', () => {
+    expect(() => {
+      stack.pop();
+    }).toThrowError('capacity underflow error');
+  });
   it.todo('pops the same one item when pushed');
   it.todo('pops two items with the most recent first');
   it.todo('accepts only a positive capacity');
