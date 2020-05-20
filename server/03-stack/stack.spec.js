d2d23374418a625667dd019542b36db813fa5f25
Regiany Almeida <regiany.nunes@ibm.com>
2020-05-19 21:57:01 -0300

    stack size is one when pushed

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,9 +1,15 @@
 const stackFactory = () => {
   let empty = true;
+  let count = 0;
+
+
   return {
     isEmpty: () => empty,
-    size: () => 0,
-    push: () => { empty = false; }
+    size: () => count,
+    push: () => {
+      empty = false;
+      count += 1;
+    }
   }
 };
 
@@ -17,6 +23,10 @@ describe('the stack canary spec', () => {
 });
 
 describe('a stack', () => {
+  beforeEach(() => { 
+    stack = stackFactory();
+  })
+
   it('starts empty', () => {
     expect(stack.isEmpty()).toBe(true);
   });
@@ -27,7 +37,10 @@ describe('a stack', () => {
     stack.push();
     expect(stack.isEmpty()).toBe(false);
   });
-  it.todo('stack size is 1 when pushed');
+  it('stack size is 1 when pushed', () => {
+    stack.push();
+    expect(stack.size()).toBe(1);
+  });
   it.todo('stack is empty when pushed and popped');
   it.todo('stack size is 0 when pushed and popped');
   it.todo('throws overflow error when pushing to a stack at full capacity');
