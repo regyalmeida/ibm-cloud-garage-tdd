69eb406ad7f78c9d4a67605b44b773103dfbfdb5
Regiany Almeida <regiany.nunes@ibm.com>
2020-05-23 00:06:06 -0300

    prime factors for 2

--- server/04-prime-factors/prime-factors.spec.js
+++ server/04-prime-factors/prime-factors.spec.js
@@ -1,4 +1,7 @@
-let findPrimeFactors = () => { return []; };
+let findPrimeFactors = (number) => { 
+  if (number === 1) return [];
+  return [number]; 
+};
 
 
 describe('the prime factors canary spec', () => {
@@ -11,7 +14,9 @@ describe('a prime factors function should', () => {
   it('return none for 1', () => {
     expect(findPrimeFactors(1)).toEqual([]);
   });
-  it.todo('return 2 for 2');
+  it('return 2 for 2', () => {
+    expect(findPrimeFactors(2)).toEqual([2]);
+  })
   it.todo('return 3 for 3');
   it.todo('return 2, 2 for 4');
   it.todo('return 5 for 5');
